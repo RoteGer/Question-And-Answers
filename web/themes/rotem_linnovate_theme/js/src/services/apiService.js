ed55656fcc4bfda38e0cb0759961349e02a5b834
RoteGer <rotem.ger207@gmail.com>
2024-07-27 14:45:20 +0300

    updated build

--- web/themes/rotem_linnovate_theme/js/src/services/apiService.js
+++ web/themes/rotem_linnovate_theme/js/src/services/apiService.js
@@ -4,6 +4,11 @@ export const fetchQuestions = async () => {
       throw new Error('Network response was not ok');
     }
     const data = await response.json();
+    
+    // sort method:
+    // If the result is negative, a comes before b.
+    // If the result is positive, a comes after b.
+    // If the result is zero, the order of a and b remains unchanged relative to each other.
     return data.data.sort((a, b) => a.attributes.field_order - b.attributes.field_order);
   };
   
